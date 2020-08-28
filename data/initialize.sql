d83d7ca26cbf313360efdda117fb72252bf6c8d5
ajewe <ajewe@utexas.edu>
2020-08-28 15:38:39 -0500

    mysql plants table created, first plant data saved to csv

--- data/initialize.sql
+++ data/initialize.sql
@@ -1,12 +1,17 @@
+DROP TABLE IF EXISTS plants;
 
 CREATE TABLE plants (
-  id INT NOT NULL AUTO_INCREMENT,
   plant_name VARCHAR(100) NOT NULL,
-  plant_spacing_per_sqft VARCHAR(10),
-  PRIMARY KEY (id)
+  plant_spacing_per_sqft VARCHAR(50),
+  PRIMARY KEY (plant_name)
 );
 
-INSERT INTO plants
-  (plant_name, plant_spacing_per_sqft)
-VALUES
-  (x, y)
+-- INSERT INTO plants
+--   (plant_name, plant_spacing_per_sqft)
+-- VALUES
+--   (x, y)
+
+-- LOAD DATA LOCAL INFILE './plantData.csv'
+-- INTO TABLE plants
+-- FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+-- LINES TERMINATED BY '/n';
--- garden-scraper.js
+++ garden-scraper.js
@@ -3,7 +3,6 @@ const puppeteer = require('puppeteer');
 const basicPlantAndSpacingScrape = (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  // let dataObj = {};
   let dataRows = [];
 
   try {
@@ -14,27 +13,12 @@ const basicPlantAndSpacingScrape = (async () => {
       return tds.map( td => td.innerText);
     })
 
-    // let plantNames = [];
-    // let plantSpacing = [];
-    
-    //arrange data into rows, each row an array
     const arrangeData = () => {
       for (let i = 0; i < dataFromPage.length; i+=2) {
-        //every even i value in array is plant name, odd i is spacing
-        // if (i % 2 === 0) {
-        //   plantNames.push(data[i]);
-        // } else {
-        //   plantSpacing.push(data[i]);
-        // }
         dataRows.push([dataFromPage[i], dataFromPage[i + 1]])
       }
       return dataRows
     };
-    console.log(arrangeData());
-    // dataObj = {
-    //   plant: plantNames,
-    //   numberOfPlants: plantSpacing,
-    // }
   } catch(e) {
     console.log(e)
   }
